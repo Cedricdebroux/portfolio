1c93fcec4ee2d18353ff48f331b168179cc82b9a
Debroux <cedric@debroux.ovh>
2022-02-11 14:02:22 +0100

    init header install bootstrap

---
+++ src/components/header/Header.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+export default class Header extends Component {
+    render() {
+        return(
+            <header className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="/">D-clic-web</a>
+                <button className="navbar-toggler">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse">
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item mr-5">
+                      Acceuil 
+                    </li>
+                    <li className="nav-item mr-5">
+                     A Propos de moi 
+                    </li>
+                    <li className="nav-item mr-5">
+                     Réalisations 
+                    </li>
+                    <li className="nav-item mr-1">
+                     Contactez moi 
+                    </li>
+                </ul>
+                </div>
+            </header>
+         );
+    }
+}
