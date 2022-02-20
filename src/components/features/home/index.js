e25b7d734a0ceee6465ead8e92117f81cc41631b
Debroux <cedric@debroux.ovh>
2022-02-20 12:34:37 +0100

    préparation of the bloc competence

--- src/components/features/home/index.js
+++ src/components/features/home/index.js
@@ -7,8 +7,8 @@ import logoW from '../../images/logoWordpress.png';
 import logoSass from '../../images/logoSass.png';
 import logoBootstrap from '../../images/logoBootstrap.png';
 import ContactMe from "../../features/contact";
-import { BrowserRouter as Router, Route, NavLink, Redirect } from 'react-router-dom'; 
-import { CSSTransition } from 'react-transition-group';
+import { NavLink } from 'react-router-dom'; 
+
 import { Nav } from 'react-bootstrap'
 
 const routes = [
@@ -24,16 +24,36 @@ class HomePage extends Component {
                     <p className={ styles.subtitle }>Votre futur</p>
                     <p className={ styles.subtitle2 }>Développeur Web Full Stack</p>
                 </div>
+
                 <img src={logoReact} alt='logo react' className={styles.logoReact} />
                 <img src={logoVue} alt='logo react' className={styles.logoVue} />
                 <img src={logoSf} alt='logo react' className={styles.logoSf} />
                 <img src={logoW} alt='logo react' className={styles.logow} />
                 <img src={logoSass} alt='logo react' className={styles.logoSass} />
                 <img src={logoBootstrap} alt='logo react' className={styles.logoBootstrap} />
+
+                <div className={ styles.box1 }>
+                    <p className={ styles.box1Title1 }>Compétences</p>
+                    <p className={ styles.box1Texte1 }>Animé d'une curiosité intellectuelle, j'aime me tenir informé des dernières nouveautés.</p>
+                    <p className={ styles.box1Texte2 }>Voici un apercu des technologies que j'utilise</p>
+                    <div className={ styles.box1Logo }>
+                        ici les logo des différentes technologies avec leur description, leur utilisation et leur liens
+                    </div>
+                    <p className={ styles.box1Texte3 }>Pour l'instant, j'apprend</p>
+                    <div className={ styles.box1Logo2 }>
+                        ici les logo des différentes technologies avec leur description, leur utilisation et leur liens
+                    </div>
+                    <p className={ styles.box1Texte4 }>Celles qui succite ma curiosité</p>
+                    <div className={ styles.box1Logo3 }>
+                        ici les logo des différentes technologies avec leur description, leur utilisation et leur liens
+                    </div>
+                </div>
+
                 <div className={ styles.box2 }>
+                    <div className={ styles.boxTitle }>
                     <p className={ styles.box2Title }>Vous voulez en savoir plus ?</p>
-                    <p className={ styles.box2Subtittle }>Contactez-moi !</p>
-                    <button>
+                    </div>
+                    <button className={ styles.formButton}>
                         {routes.map(route => (
                         <Nav.Link
                         key={route.path}
@@ -41,12 +61,12 @@ class HomePage extends Component {
                         to={route.path}
                         activeClassName="active"
                         exact
+                        className={ styles.navLink }
                         >
                                 {route.name}
                         </Nav.Link>
                             ))} 
                     </button>
-                    
                 </div>
                 
             </>
