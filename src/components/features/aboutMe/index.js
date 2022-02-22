fa3431f05767f9d16face094d9e0dbae1eac72c7
Debroux <cedric@debroux.ovh>
2022-02-22 14:15:37 +0100

    before download file, text in about me in progress

--- src/components/features/aboutMe/index.js
+++ src/components/features/aboutMe/index.js
@@ -1,12 +1,54 @@
 import React, { Component } from "react";
 import styles from "./AboutMe.module.css";
+import { Nav } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import CV from '../../images/cv/cedric_debroux.pdf';
 
 export default class AboutMe extends Component {
   render() {
     return (
       <>
         <div>
-          <p className={ styles.title }>A propos de moi</p>
+          <h1 className={ styles.title }>
+            Il était une fois
+          </h1>
+          <p className={ styles.text }>
+            Fruit des années 80, passioné par le monde numérique depuis mon enfance.
+          </p>
+          <p className={ styles.text }>
+           Mes premiers pas dans le monde de l'informatique se sont faits sur une machine tournant sous DOS et depuis je suis resté addicte.
+          </p>
+          <p className={ styles.text }>
+            Après une reconversion professionnelle réussie dans le développement web, je prends plaisir à évoluer dans ce nouvel environement en perpétuel changement.
+          </p>
+
+          <h2 className={ styles.title }>
+            Aujourd'hui
+          </h2>
+          <p className={ styles.text }>
+           Je suis avide de découvrir de nouvelles technologies et de m'y perfectionner grâce à mon besoin d'apprendre au quotidien.
+          </p>
+          <p className={ styles.text }>
+            Mes capacités de communication et d'adaptation viennent de ma nature empathique.
+          </p>
+          <p className={ styles.text }>
+            Étant autonome et proactif, j'aime prendre des initiatives pour atteindre mes objectifs. 
+          </p>
+          <p className={ styles.text }>
+            Le travail en équipe représente toujours une opportunité d'échange et de découverte <em>"Seul, on va plus vite, ensemble, on va plus loin"</em>.
+          </p>
+          <p className={ styles.text }>
+            Je serai ravi de vous rencontrer pour vous en dire plus.
+          </p>
+          <button className={ styles.formButton}>
+              <Nav.Link as={NavLink} className={ styles.navLink } to="/contact">Contactez-moi</Nav.Link>
+          </button>
+          {/* contactez-moi ! bouton  */}
+
+
+          <h2 className={ styles.title }>
+            À l'horizon
+          </h2>
         </div>
       </>
     );
