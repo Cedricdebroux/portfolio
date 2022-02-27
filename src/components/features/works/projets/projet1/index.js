f8fe359fd563e09dd9733441251c496c97d53450
Debroux <cedric@debroux.ovh>
2022-02-27 14:09:13 +0100

    movies ok

--- src/components/features/works/projets/projet1/index.js
+++ src/components/features/works/projets/projet1/index.js
@@ -5,6 +5,10 @@ import { Nav } from 'react-bootstrap';
 import back from '../../../../images/pictos/back.png';
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import '@splidejs/splide/dist/css/splide.min.css';
+import page1 from '../../../../images/works/movies/page1.webp';
+import page2 from '../../../../images/works/movies/page2.webp';
+import page3 from '../../../../images/works/movies/page3.webp';
+import page4 from '../../../../images/works/movies/page4.webp';
 
 
 function Projet1() {
@@ -16,14 +20,24 @@ function Projet1() {
      <div className={ styles.containerProjet1 }>
         <h1 className={ styles.title }>Projet Film</h1>
         <h2 className={ styles.subTitle }>
-          Projet React
+           React
         </h2>
         <div className={ styles.content }>
           <div className={ styles.boxText }>
             <p className={ styles.text}>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum soluta eius fugit atque, iste omnis corrupti laboriosam, commodi nobis placeat nihil, itaque non nesciunt voluptate delectus ea accusamus blanditiis architecto!
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum soluta eius fugit atque, iste omnis corrupti laboriosam, commodi nobis placeat nihil, itaque non nesciunt voluptate delectus ea accusamus blanditiis architecto!  
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum soluta eius fugit atque, iste omnis corrupti laboriosam, commodi nobis placeat nihil, itaque non nesciunt voluptate delectus ea accusamus blanditiis architecto!  
+              Après plusieurs mois passés sur Symfony, c'était l'occasion pour moi de me remettre à un frameworks javascript front-end.
+            </p>
+            <p className={ styles.text}>
+              J'ai pris beaucoup de plaisir à réaliser celui-ci.
+            </p>
+            <p className={ styles.text}>
+              Il a été réalisé en React avec notament Formik, React-Router, Axios et Bootstrap.
+            </p>
+            <p className={ styles.text}>
+              Il a une barre de recherche fonctionelle avec la selection de la langue du film à afficher dans le résultat, et un ajout au favoris de vos films préférés.
+            </p>
+            <p className={ styles.text}>
+              Pour l'instant, il n'est pas encore relié à une base de données, mais ce projet est encore amené à évoluer.
             </p>
           </div>
           <div className={ styles.images }>
@@ -38,13 +52,16 @@ function Projet1() {
               } }
             >
               <SplideSlide>
-                <img className={ styles.images } src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt=""/>
+                <img className={ styles.images } src={page1} alt="page 1 movies"/>
               </SplideSlide>
               <SplideSlide>
-                <img className={ styles.images } src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt=""/>
+                <img className={ styles.images } src={page2} alt="page2 movies"/>
               </SplideSlide>
               <SplideSlide>
-                <img className={ styles.images } src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt=""/>
+                <img className={ styles.images } src={page3} alt="page3 movies"/>
+              </SplideSlide>
+              <SplideSlide>
+                <img className={ styles.images } src={page4} alt="page4 movies"/>
               </SplideSlide>
             </Splide>
           </div>
