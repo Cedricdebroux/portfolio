718b4c2b711428e3ddd0c7a3b731ffe9dab175a1
Debroux <cedric@debroux.ovh>
2022-02-27 10:21:53 +0100

    page contact refactor title and subtitle

--- src/components/features/contact/index.js
+++ src/components/features/contact/index.js
@@ -35,81 +35,80 @@ if (name.length === 0 || phone.length === 0 || email.length === 0 || message.len
     };}
     
     return (
-      <div className={styles.formCustom }>
-      <form ref={form} onSubmit={sendEmail} className="contact-form ">
-        <div className={ styles.emailBox }>
-            <h2 className={ styles.formH2 }>Contactez-moi</h2>
-            <img src={mail} alt='email' className={styles.emailIcon} />
-        </div>
-          <div className="mb-3">
-          <input
-            className='form-control border-bottom-1 border-left-0 border-right-0 border-top-0'
-            type="text"
-            id="name"
-            name="name"
-            onChange={(e) => setName(e.target.value)}
-            placeholder="Nom *"
-            value={name}
-            autoComplete="off"
-          />
+      <>
+      <h1 className={ styles.title }>
+        Contactez-moi
+      </h1>
+      <div className={styles.formCustom}>
+          <form ref={form} onSubmit={sendEmail} className="contact-form ">
+            <div className={styles.emailBox}>
+              <h2 className={styles.formH2}>Travaillons ensemble</h2>
+              <img src={mail} alt='email' className={styles.emailIcon} />
+            </div>
+            <div className="mb-3">
+              <input
+                className='form-control border-bottom-1 border-left-0 border-right-0 border-top-0'
+                type="text"
+                id="name"
+                name="name"
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Nom *"
+                value={name}
+                autoComplete="off" />
+            </div>
+            <div className="mb-3">
+              <input
+                className='form-control border-bottom-1 border-left-0 border-right-0 border-top-0'
+                type="text"
+                id="company"
+                name="company"
+                onChange={(e) => setCompany(e.target.value)}
+                placeholder="Société"
+                value={company} />
+            </div>
+            <div className="mb-3">
+              <input
+                className='form-control border-bottom-1 border-left-0 border-right-0 border-top-0'
+                type="text"
+                id="phone"
+                name="phone"
+                onChange={(e) => setPhone(e.target.value)}
+                placeholder="Téléphone"
+                value={phone} />
+            </div>
+            <div className="mb-3">
+              <input
+                className='form-control border-bottom-1 border-left-0 border-right-0 border-top-0'
+                type="mail"
+                id="email"
+                name="email"
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email *"
+                value={email}
+                autoComplete="on" />
+            </div>
+            <div className="mb-3">
+              <textarea
+                className='form-control border-bottom-1 border-left-0 border-right-0 border-top-0'
+                id="message"
+                name="message"
+                onChange={(e) => setMessage(e.target.value)}
+                placeholder="Votre message *"
+                value={message} />
+            </div>
+            <input type="submit" value="Envoyer" className={styles.formButton} />
+          </form>
+          <div className={styles.sendValid}>
+            <p className="send-valid"></p>
           </div>
-          <div className="mb-3">
-          <input
-            className='form-control border-bottom-1 border-left-0 border-right-0 border-top-0'
-            type="text"
-            id="company"
-            name="company"
-            onChange={(e) => setCompany(e.target.value)}
-            placeholder="Société"
-            value={company}
-          />
+          <div className={styles.sendInvalid}>
+            <p className="send-invalid"></p>
           </div>
-          <div className="mb-3">
-          <input
-            className='form-control border-bottom-1 border-left-0 border-right-0 border-top-0'
-            type="text"
-            id="phone"
-            name="phone"
-            onChange={(e) => setPhone(e.target.value)}
-            placeholder="Téléphone"
-            value={phone}
-          />
+          <div className={styles.sendInvalidInput}>
+            <p className="send-invalid-input"></p>
           </div>
-          <div className="mb-3">
-            <input
-              className='form-control border-bottom-1 border-left-0 border-right-0 border-top-0'
-              type="mail"
-              id="email"
-              name="email"
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder="Email *"
-              value={email}
-              autoComplete="on"
-            />
-          </div>
-          <div className="mb-3">
-          <textarea
-            className='form-control border-bottom-1 border-left-0 border-right-0 border-top-0'
-            id="message"
-            name="message"
-            onChange={(e) => setMessage(e.target.value)}
-            placeholder="Votre message *"
-            value={message}
-          />
-          </div>
-        <input type="submit" value="Envoyer" className={ styles.formButton } />
-      </form>
-      <div className={ styles.sendValid}>
-        <p className="send-valid"></p>
-      </div>
-      <div className={ styles.sendInvalid}>
-        <p className="send-invalid"></p>
-      </div>
-      <div className={ styles.sendInvalidInput}>
-        <p className="send-invalid-input"></p>
-      </div>
 
-    </div>
+        </div></>
   );
 };
 export default ContactMe;
