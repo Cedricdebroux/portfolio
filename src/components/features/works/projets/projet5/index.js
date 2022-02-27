47d19e38ab405771bcf2d239218e21999c4c718b
Debroux <cedric@debroux.ovh>
2022-02-27 15:22:04 +0100

    forum

--- src/components/features/works/projets/projet5/index.js
+++ src/components/features/works/projets/projet5/index.js
@@ -5,6 +5,10 @@ import { Nav } from 'react-bootstrap';
 import back from '../../../../images/pictos/back.png';
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import '@splidejs/splide/dist/css/splide.min.css';
+import login from '../../../../images/works/forum/login.webp';
+import page1 from '../../../../images/works/forum/page1.webp';
+import page2 from '../../../../images/works/forum/page2.webp';
+import page3 from '../../../../images/works/forum/page3.webp';
 
 
 function Projet5() {
@@ -14,16 +18,26 @@ function Projet5() {
        <img src={back} alt="back" className={styles.back}/>
      </Nav.Link>
      <div className={ styles.containerProjet1 }>
-        <h1 className={ styles.title }>Projet Film</h1>
+        <h1 className={ styles.title }>Forum</h1>
         <h2 className={ styles.subTitle }>
-          Projet React
+          Un forum complet en PHP
         </h2>
         <div className={ styles.content }>
           <div className={ styles.boxText }>
             <p className={ styles.text}>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum soluta eius fugit atque, iste omnis corrupti laboriosam, commodi nobis placeat nihil, itaque non nesciunt voluptate delectus ea accusamus blanditiis architecto!
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum soluta eius fugit atque, iste omnis corrupti laboriosam, commodi nobis placeat nihil, itaque non nesciunt voluptate delectus ea accusamus blanditiis architecto!  
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum soluta eius fugit atque, iste omnis corrupti laboriosam, commodi nobis placeat nihil, itaque non nesciunt voluptate delectus ea accusamus blanditiis architecto!  
+            Ce projet a été réalisé en équipe dans le cadre de ma formation chez Becode.
+            </p>
+            <p className={ styles.text}>
+            L'enjeu était de réaliser un forum complet en PHP.
+            </p>
+            <p className={ styles.text}>
+            Pour ce dernier, nous avons une page de login et signup à l'arriver sur le site.
+            </p>
+            <p className={ styles.text}>
+            Ensuite, plusieurs informations devaient apparaître sur la page d'accueil, notamment les derniers utilisateurs connecter, les derniers posts classés par catégories.
+            </p>
+            <p className={ styles.text}>
+            Pour en savoir plus je vous invite à visiter le <a href="https://github.com/Sanamanel/Forum" target="blank">GitHub</a>.
             </p>
           </div>
           <div className={ styles.images }>
@@ -38,13 +52,16 @@ function Projet5() {
               } }
             >
               <SplideSlide>
-                <img className={ styles.images } src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt=""/>
+                <img className={ styles.images } src={ login } alt="login"/>
               </SplideSlide>
               <SplideSlide>
-                <img className={ styles.images } src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt=""/>
+                <img className={ styles.images } src={ page1 } alt="page1"/>
               </SplideSlide>
               <SplideSlide>
-                <img className={ styles.images } src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt=""/>
+                <img className={ styles.images } src={ page2 } alt="page2"/>
+              </SplideSlide>
+              <SplideSlide>
+                <img className={ styles.images } src={ page3 } alt="page3"/>
               </SplideSlide>
             </Splide>
           </div>
