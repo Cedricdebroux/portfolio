7db54f4d9b7b896503eee2749bd880c93bdbc615
Debroux <cedric@debroux.ovh>
2022-03-01 13:25:55 +0100

    size of gif loading

--- src/components/utils/Loading.js
+++ src/components/utils/Loading.js
@@ -7,7 +7,7 @@ function Loading() {
     return(
       <div className={ styles.loading }>
         <div style={{ minHeight: '100vh' }} className="d-flex flex-row justify-content-center align-items-center w-100">
-          <img alt="loading gif" className={ styles.loadingImg } src={GifLoading} />
+          <img alt="loading gif" className={ styles.loadingImg } src={GifLoading} width="500" height="500" />
         </div>
       </div>
     );
