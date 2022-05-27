805087ce4fdf45705d7091b12d207760fb3f44fa
Debroux <cedric@debroux.ovh>
2022-05-27 15:09:48 +0200

    avant modifications réalisations

--- src/components/features/aboutMe/index.js
+++ src/components/features/aboutMe/index.js
@@ -11,76 +11,76 @@ import { withTranslation } from 'react-i18next';
 
 class AboutMe extends Component {
   render() {
-  const saveFile = () => {
-    saveAs(
-      Cv,
-      "cedric_debroux.pdf"
-    );
-  };
+    const saveFile = () => {
+      saveAs(
+        Cv,
+        "cedric_debroux.pdf"
+      );
+    };
     return (
       <>
-        <div className={ styles.aboutMe}>
-          <h1 className={ styles.title }>
-            { this.props.t('aboutMe.title') }
+        <div className={styles.aboutMe}>
+          <h1 className={styles.title}>
+            {this.props.t('aboutMe.title')}
           </h1>
-          <p className={ styles.text }>
-            { this.props.t('aboutMe.text1') }
+          <p className={styles.text}>
+            {this.props.t('aboutMe.text1')}
           </p>
-          <p className={ styles.text }>
-            { this.props.t('aboutMe.text2') }
+          <p className={styles.text}>
+            {this.props.t('aboutMe.text2')}
           </p>
-          <p className={ styles.text }>
-            { this.props.t('aboutMe.text3') }
+          <p className={styles.text}>
+            {this.props.t('aboutMe.text3')}
           </p>
 
-          <h2 className={ styles.title }>
-            { this.props.t('aboutMe.title2') }
+          <h2 className={styles.title}>
+            {this.props.t('aboutMe.title2')}
           </h2>
-          <p className={ styles.text }>
-            { this.props.t('aboutMe.text4') }
+          <p className={styles.text}>
+            {this.props.t('aboutMe.text4')}
           </p>
-          <p className={ styles.text }>
-            { this.props.t('aboutMe.text5') }
+          <p className={styles.text}>
+            {this.props.t('aboutMe.text5')}
           </p>
-          <p className={ styles.text }>
-            { this.props.t('aboutMe.text6') } 
+          <p className={styles.text}>
+            {this.props.t('aboutMe.text6')}
           </p>
-          <p className={ styles.text }>
+          <p className={styles.text}>
             {this.props.t('aboutMe.text7')}<em>{this.props.t('aboutMe.text7Em')}</em>.
           </p>
-          <p className={ styles.text }>
-            { this.props.t('aboutMe.text8') }
+          <p className={styles.text}>
+            {this.props.t('aboutMe.text8')}
           </p>
-          <Nav.Link as={NavLink} className={ styles.navLink } to="/contact">
-          <button className={ styles.formButton}>
-            { this.props.t('home.buttonContactMe') }
-          </button>
+          <Nav.Link as={NavLink} className={styles.navLink} to="/contact">
+            <button className={styles.formButton}>
+              {this.props.t('home.buttonContactMe')}
+            </button>
           </Nav.Link>
 
-          <h2 className={ styles.titleH }>
-            { this.props.t('aboutMe.title3') }
+          <h2 className={styles.titleH}>
+            {this.props.t('aboutMe.title3')}
           </h2>
-          <div className={ styles.logoHorizon }>
-            <img className={ styles.logoH1 } src={logoDart} alt="logo Dart" width="30" heigh="30"/>
-            <img className={ styles.logoH2 } src={logoFlutter} alt="logo Flutter" width="30" heigh="30"/>
-            <img className={ styles.logoH3 } src={logoJava} alt="logo Java" width="30" heigh="30"/>
+          <div className={styles.logoHorizon}>
+            <img className={styles.logoH1} src={logoDart} alt="logo Dart" width="30" heigh="30" />
+            <img className={styles.logoH2} src={logoFlutter} alt="logo Flutter" width="30" heigh="30" />
+            <img className={styles.logoH3} src={logoJava} alt="logo Java" width="30" heigh="30" />
           </div>
-          <p className={ styles.text }>
-            { this.props.t('aboutMe.text9') }
+          <p className={styles.text}>
+            {this.props.t('aboutMe.text9')}
           </p>
-          <p className={ styles.text }>
-            { this.props.t('aboutMe.text10') }
+          <p className={styles.text}>
+            {this.props.t('aboutMe.text10')}
           </p>
-          <p className={ styles.text }>
-            { this.props.t('aboutMe.text11') }
+          <p className={styles.text}>
+            {this.props.t('aboutMe.text11')}
           </p>
-          <p className={ styles.text }>
-            { this.props.t('aboutMe.text12') } 
+          <p className={styles.text}>
+            {this.props.t('aboutMe.text12')}
           </p>
-          <p className={ styles.text }>
-            { this.props.t('aboutMe.text13') }
+          <p className={styles.text}>
+            {this.props.t('aboutMe.text13')}
           </p>
-          <button onClick={saveFile} className={ styles.formButtonDown}>{this.props.t('aboutMe.download')}</button>
+          <button onClick={saveFile} className={styles.formButtonDown}>{this.props.t('aboutMe.download')}</button>
         </div>
       </>
     );
